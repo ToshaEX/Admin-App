cd3507939b78c5105147c816af0a2323b156f0cf
ToshaEX <tharinduoshan@gmail.com>
2022-04-06 13:52:59 +0530

    Complete Sidebar Implementation

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import "./SideBar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import StoreIcon from "@mui/icons-material/Store";
+import FactCheckIcon from "@mui/icons-material/FactCheck";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 function SideBar() {
   return (
@@ -7,25 +19,70 @@ function SideBar() {
       <div className="top">
         <span className="logo">lamadmin</span>
       </div>
-      <hr/>
+      <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
+          <p className="title">LIST</p>
           <li>
-            <span>Dashboard</span>
+            <AccountCircleIcon className="icon" />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <StoreIcon className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <FactCheckIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <QueryStatsIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <HealthAndSafetyIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountBoxIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon" />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
 
-      <div className="bottom">color option</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   );
 }
